7a632d531e7c48e3263970a2da353f5337999da1
Yaroslav Smirnov <yaroslav.smirnov@team.wrike.com>
2017-04-17 14:06:40 +0300

    Initial commit

---
+++ TrueKeep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrueKeep
+//
+//  Created by Yaroslav Smirnov on 17/04/2017.
+//  Copyright © 2017 Yaroslav Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
